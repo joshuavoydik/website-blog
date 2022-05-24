35e7b38ff9b3225f1ce3ab3b4d17aba1fc24b8b3
Joshua <voydik@gmail.com>
2022-05-24 09:27:25 -0700

    Changed some useful metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  author: 'Joshua Voydik',
+  headerTitle: 'Joshua Voydik',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
